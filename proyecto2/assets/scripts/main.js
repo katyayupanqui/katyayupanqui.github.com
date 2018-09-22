38e92a2324cf54c5ce2886b2ad144bc9da89d603
katyayupanqui <42705900+katyayupanqui@users.noreply.github.com>
2018-09-21 22:57:48 -0300

    Update main.js

--- proyecto2/assets/scripts/main.js
+++ proyecto2/assets/scripts/main.js
@@ -30,7 +30,7 @@ closes.forEach(function(close){
         content.classList.add("animated");
         content.classList.add("fadeOutRight");
         setTimeout(() => {
-            location.href="file:///C:/Users/Katy/Documents/aiep/katyayupanqui.github.com/proyecto2/index.html";
+            location.href="https://katyayupanqui.github.io/proyecto2/";
         }, 1000);
     })
 
